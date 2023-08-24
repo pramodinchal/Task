0cd3ab57d30a328a389f80ae75fabba91d8a9241
Pramod Inchal <inchalpramod@gmail.com>
2023-08-25 03:32:29 +0530

    first commit

--- src/components/ManageProject.js
+++ src/components/ManageProject.js
@@ -1,34 +1,40 @@
-import React from 'react'
+import React from "react";
 
 const ManageProject = () => {
   return (
-    <div className='container4'>
-        <div className="header">
+    <div className="container4">
+      <div className="header">
         <h6>Who can manage projects</h6>
         <p>Don't panic - You can also customize this types in settings</p>
-    </div>
-    <div className='blocks'>
-    <div id='block1' style={{ display: 'block', alignItems: 'center' }}>
-        <img src="everyone.png" alt="My Image" id="imgeveryon"/>
-        <span id='span'>Everyone</span><p id='p'>All users can now see to it,but guests cannot <br/> access the projects.</p>
       </div>
+      <div className="blocks">
+        <div id="block1" style={{ display: "block", alignItems: "center" }}>
+          <img src="everyone.png" alt="My Image" id="imgeveryon" />
+          <span id="span">Everyone</span>
+          <p id="p">
+            All users can now see to it,but guests cannot <br /> access the
+            projects.
+          </p>
+        </div>
+      </div>
+      <div className="block2">
+        <div style={{ display: "block", alignItems: "center" }}>
+          <img src="admin1.png" alt="My Image" id="imgadmin" />
+          <span id="span">Only Admin's</span>
+          <p id="p1">Only admins can manage everything.</p>
+        </div>
+      </div>
+      <div className="block3">
+        <div style={{ display: "block", alignItems: "center" }}>
+          <img src="specific.png" alt="My Image" id="imgspecific" />
+          <span id="span">Only to Specific people</span>
+          <p id="p1">Only some specific people can be able to see.</p>
+        </div>
+      </div>
+      <i class="fa-solid fa-chevron-left"></i>
+      <button id="nxtbtn4">Next</button>
     </div>
-    <div className='block2'>
-         <div style={{ display: 'block', alignItems: 'center' }}>
-        <img src="admin1.png" alt="My Image" id="imgadmin"/>
-        <span id='span'>Only Admin's</span><p id='p1'>Only admins can manage everything.</p>
-      </div>  
-    </div>
-    <div className='block3'>
-         <div style={{ display: 'block', alignItems: 'center' }}>
-        <img src="specific.png" alt="My Image" id="imgspecific"/>
-        <span id='span'>Only to Specific people</span><p id='p1'>Only some specific people can be able to see.</p>
-      </div>  
-    </div>
-    <i class="fa-solid fa-chevron-left"></i>
-    <button id="nxtbtn4">Next</button>
-    </div>
-  )
-}
+  );
+};
 
 export default ManageProject;
--- src/components/SelectView.js
+++ src/components/SelectView.js
@@ -1,20 +1,24 @@
-import React from 'react'
+import React from "react";
 /* import image from "./listicon.png" */
 
 const SelectView = () => {
   return (
-    <div className='container3'>
-     <div className="header">
-      <h6>Select a view</h6>
-      <p>You can also customize this types in settings</p>
-    </div>
-     <div>
-      <img src="listicon.png" alt="image-list" id='list'/> <img src="board.png" alt="image-list" id='list'/>
-     <div><span id="spanlist">List</span><span id='spanboard'>Board</span></div>
-      </div> 
+    <div className="container3">
+      <div className="header">
+        <h6>Select a view</h6>
+        <p>You can also customize this types in settings</p>
+      </div>
+      <div>
+        <img src="listicon.png" alt="image-list" id="list" />{" "}
+        <img src="board.png" alt="image-list" id="list" />
+        <div>
+          <span id="spanlist">List</span>
+          <span id="spanboard">Board</span>
+        </div>
+      </div>
       <button id="nxtbtn3">Next</button>
     </div>
-  )
-}
+  );
+};
 
 export default SelectView;
